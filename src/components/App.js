1df0be188f2ac327bec6cc8f3b9bc2349391c860
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-05 14:56:37 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  let jsx=<p>I am learning React. My life is getting better.</p>
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div id="root">
+        {jsx}
+      </div>
+    </div>
   )
 }
 
